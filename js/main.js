014b108d24a8903488c06e4f584e6d471c3914f7
jaloliddinbegmamatov08 <jaloliddinbegmamatov08@gmail.com>
2024-02-05 01:17:03 +0500

    2-oy Exam

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+/*animation*/
+const loading = document.getElementById("loading");
+window.addEventListener("load", () => {
+  loading.classList.add("loading-none");
+});
